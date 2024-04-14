5b0c21fcb7c133cb51414f293bc419f65c8b02e5
udaysakshemit123 <uday.mathur@sakshemit.com>
2024-04-15 01:09:36 +0530

    Add project files.

---
+++ CleaningWebsite/Controllers/CleaningController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CleaningWebsite.Controllers
+{
+    public class CleaningController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Service()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
